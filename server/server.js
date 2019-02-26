57c0ed2453a24a57e4f4e7ed9880adcb47124958
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-02-26 13:25:48 -0600

    setup apollo client and SongList component

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://ethan:ethan1@ds153495.mlab.com:53495/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
