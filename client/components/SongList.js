db3a7b24cd214bac10e845be13cc747dc170345e
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-03-03 10:54:55 -0600

    setup react router

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,10 +3,20 @@ import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className='collection-item'>{song.title}</li>
+      )
+    })
+  }
+
   render() {
+    if (this.props.data.loading) return <div></div>
+
     return (
-      <div>
-        SongList
+      <div className='collection'>
+        {this.renderSongs()}
       </div>
     )
   }
@@ -15,6 +25,7 @@ class SongList extends Component {
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
